02f612c7428f4d86401a547f05efc39c24c36258
MelvinJohn00 <melvinjohnv2000@gmail.com>
2024-10-20 09:01:06 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringJavaMailApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import com.example.demo.mailService.JavaMailerService;
+
+@SpringBootApplication
+public class SpringJavaMailApplication {
+	
+	@Autowired
+	private JavaMailerService jm;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaMailApplication.class, args);
+		
+	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void send() {
+		jm.sendMail("www.melvinjohnv00@gmail.com", "testmail", "Testing Content for mail-Spring-Java Mailer Project ");
+	}
+
+}
